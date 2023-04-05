8dd6d2faf23e4dad21109e44db57b968e43055d1
Onur Güneç <ogunec@hotmail.com>
2023-04-05 23:54:34 +0300

    customercontroller added

---
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
